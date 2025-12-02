63ba1825e37a61c4d2c868095805d4c343c27b26
aprove <34180766+aprove012@users.noreply.github.com>
2025-12-02 20:33:16 +0900

    Fix: Global_EF 로칼 저장

--- lib/feedback_module.dart
+++ lib/feedback_module.dart
@@ -2,47 +2,46 @@ import 'dart:math';
 import 'memory_item.dart';
 
 class FeedbackModule {
-  MemoryItem schedule(MemoryItem item, int q) {
-    assert(q >= 0 && q <= 5, 'Feedback quality (q) must be between 0 and 5.');
+    MemoryItem schedule(MemoryItem item, int q) {
+        assert(q >= 0 && q <= 5, 'Feedback quality (q) must be between 0 and 5.');
 
-    double oldEF = item.ef;
-    double qDouble = q.toDouble();
-    double newEF =
-        oldEF + (0.1 - (5.0 - qDouble) * (0.08 + (5.0 - qDouble) * 0.02));
+        double oldEF = item.ef;
+        double qDouble = q.toDouble();
+        double newEF =
+            oldEF + (0.1 - (5.0 - qDouble) * (0.08 + (5.0 - qDouble) * 0.02));
 
-    newEF = max(1.3, newEF);
+        newEF = max(1.3, newEF);
 
-    int newRepetitions;
-    int newInterval;
+        int newRepetitions;
+        int newInterval;
 
-    if (q < 3) {
-      newRepetitions = item.repetitions;
+        if (q < 3) {
+            newRepetitions = item.repetitions;
 
-      int newIntervalCalc = (item.interval * 0.5).round();
-      newInterval = max(1, newIntervalCalc);
-      
-    } else {
-      newRepetitions = item.repetitions + 1;
+            int newIntervalCalc = (item.interval * 0.5).round();
+            newInterval = max(1, newIntervalCalc);
 
-      if (item.interval == 0) {
-        newInterval = 1;
-      } else {
-        newInterval = (item.interval * newEF).round();
-      }
+        } else {
+            newRepetitions = item.repetitions + 1;
 
-      if (newInterval < 1) {
-        newInterval = 1;
-      }
+            if (item.interval == 0) {
+                newInterval = 1;
+            } else {
+                newInterval = (item.interval * newEF).round();
+            }
+
+            if (newInterval < 1) {
+                newInterval = 1;
+            }
+        }
+
+        DateTime newNextReviewDate = DateTime.now().add(Duration(seconds: newInterval*10));
+
+        return item.copyWith(
+            ef: newEF,
+            interval: newInterval,
+            repetitions: newRepetitions,
+            nextReviewDate: newNextReviewDate,
+        );
     }
-
-    DateTime newNextReviewDate = DateTime.now().add(Duration(seconds: newInterval*10));
-
-    return item.copyWith(
-      ef: newEF,
-      interval: newInterval,
-      repetitions: newRepetitions,
-      nextReviewDate: newNextReviewDate,
-    );
-  }
 }
-
